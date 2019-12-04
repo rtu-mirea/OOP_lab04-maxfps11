f2a6d6ca20ff8aa6f7f316dea46c208e434053ad
maxfps11 <super.vbif201@ya.ru>
2019-12-04 16:03:30 +0300

    Update

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.io;
+import java.io.File;
+
+class Main {
+    public static void main(String[] args) {
+        // first Exercise
+        
+        File file1 = new File("Hello.txt");
+    }
+}
