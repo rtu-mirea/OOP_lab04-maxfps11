eae06fe36c93dae9c10bbf1a9dba4fe8b8c0eafc
maxfps11 <super.vbif201@ya.ru>
2019-12-04 20:04:19 +0300

    Update

--- Main.java
+++ Main.java
@@ -1,41 +1,46 @@
 import java.io;
 import java.io.File;
+import java.lang.Exception;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
 class Main {
     public static void main(final String[] args) {
+        final String diskCRootPath = "C:/";
+        final String usersFoldrPath = "C:/Users/";
         final String userHomePath = System.getProperty("user.home");
         final String OOP_lab04Path = userHomePath + "/Documents/VSCodeProjects/Java/OOP_lab04-maxfps11/";
 
-        // Create the MyFile1
-        final File MyFile1 = new File(OOP_lab04Path, "MyFile1.txt");
+        try {
+            // Create the MyFile1
+            File f1 = new File(OOP_lab04Path, "MyFile1.txt");
+            //check(f1.createNewFile());
 
-        out("Проверяем, существует ли MyFile1.txt по пути " + OOP_lab04Path);
+            // Create the MyFile2
+            File f2 = new File(diskCRootPath, "MyFile2.txt");
+            //check(f2.createNewFile());
 
-        if (!MyFile1.isFile()) {
-            out("Файл отсутствует.");
-            try {
-                out("Попытка создания файла.");
-                MyFile1.createNewFile();
-            } catch (FileNotFoundException e) {
-                out(e.getMessage());
-            } catch (IOException e) {
-                out(e.getMessage());
-            } catch (Exception e) {
-                out(e.getMessage());
-            } finally {
-                // Checking the creation of MyFile1.
-                if (MyFile1.isFile())
-                    out("Файл успешно создан и расположен по " + MyFile1.getPath());
-                else
-                    out("Файл не был создан.");
-            }
-        } else {
-            out("Подобный файл существует.");
+            // Create the MyFile3
+            File f3 = new File(usersFoldrPath, "MyFile3.txt");
+            //check(f3.createNewFile());
+        
+            //Create third level folder
+            File f4 = new File(OOP_lab04Path, "third");
+            //check(f4.mkdir());
+        } catch (FileNotFoundException e) {
+            out(e.getMessage());
+        } catch (IOException e) {
+            out(e.getMessage());
+        } catch (Exception e) {
+            out(e.getMessage());
         }
+    }
 
-
+    private static void check(boolean res) {
+        if (res)
+            out("Успешно");
+        else
+            out("Неуспешно");
     }
 
     private static <T> void out(T output) {
